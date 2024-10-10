dbed4b5b2e3af3baf04c71e58df3d868004cb5aa
GustavoSantannaKuster <156707143+GustavoSantannaKuster@users.noreply.github.com>
2024-10-10 14:56:25 -0300

    Contagem de letras 'a' em uma string.py

--- ex2.py
+++ ex2.py
@@ -1,27 +1,8 @@
-def verifica_fibonacci(numero):
-    # Iniciando os dois primeiros números da sequência de Fibonacci
-    a, b = 0, 1
+def count_letter_a(string):
+    count = string.lower().count('a')
+    return f"A letra 'a' aparece {count} vezes na string."
 
-    # Verifica se o número é igual a um dos dois primeiros números da sequência
-    if numero == a or numero == b:
-        return True
+# Exemplo de uso
+string = input("Informe uma string: ")
+print(count_letter_a(string))
 
-    # Calcula a sequência de Fibonacci até que o próximo número seja maior que o número informado
-    while b < numero:
-        a, b = b, a + b
-
-        # Se o próximo número na sequência for igual ao número informado, retorna True
-        if b == numero:
-            return True
-
-    # Se o número informado não pertence à sequência de Fibonacci, retorna False
-    return False
-
-
-# Número a ser verificado se pertence à sequência de Fibonacci
-numero = int(input("Informe um número para verificar se pertence à sequência de Fibonacci: "))
-
-if verifica_fibonacci(numero):
-    print(f"O número {numero} pertence à sequência de Fibonacci.")
-else:
-    print(f"O número {numero} não pertence à sequência de Fibonacci.")
